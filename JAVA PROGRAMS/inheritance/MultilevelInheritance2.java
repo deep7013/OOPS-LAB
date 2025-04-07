eff3ae2df9f185a2841e03fe9225f8e060f3c4ae
deep7013 <dhanalakshmig431@gmail.com>
2025-04-07 10:46:52 +0530

    Add files via upload

---
+++ JAVA PROGRAMS/inheritance/MultilevelInheritance2.java
@@ -0,0 +1,28 @@
+
+
+class Device {
+    void powerOn() {
+        System.out.println("Device is powered on.");
+    }
+}
+
+class Computer extends Device {
+    void runSoftware() {
+        System.out.println("Computer is running software.");
+    }
+}
+
+class Laptop extends Computer {
+    void fold() {
+        System.out.println("Laptop can be folded.");
+    }
+}
+
+public class MultilevelInheritance2 {
+    public static void main(String[] args) {
+        Laptop myLaptop = new Laptop();
+        myLaptop.powerOn();   
+        myLaptop.runSoftware();
+        myLaptop.fold();       
+    }
+}
