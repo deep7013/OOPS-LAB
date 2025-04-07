0dd032c80892f15914e845af9f2963215390e566
deep7013 <dhanalakshmig431@gmail.com>
2025-04-07 10:57:25 +0530

    Add files via upload

---
+++ JAVA PROGRAMS/POLYMORPHISM/Student.java
@@ -0,0 +1,18 @@
+class Student {
+    String name;
+    int age;
+
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Navadeep", 18);
+        s1.display();
+    }
+}
